087979716d8dcb2a1fb333f16096401db65a2306
shahin2525 <shahin451999@gmail.com>
2025-09-02 23:59:41 +0600

    add cloudniry

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -15,4 +15,7 @@ export default {
   refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_access_expire_in: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_expire_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+  cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
